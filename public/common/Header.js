d8e091081d3d79a035e79d7a34b6120079c10283
Nick <nickbrimmer@gmail.com>
2019-11-21 15:44:48 -0800

    fixed the logged in as user

--- public/common/Header.js
+++ public/common/Header.js
@@ -3,13 +3,21 @@ import Component from '../Component.js';
 class Header extends Component {
     onRender(dom) {
         if (localStorage.getItem('USER')) {
+            const user = JSON.parse(localStorage.getItem('USER'));
+            //USER.displayName
+            const displayUserName = dom.querySelector('.logged-in-as');
+            displayUserName.textContent = `Logged in as ${user.displayName}`;
+            
             const button = dom.querySelector('.log-out');
             button.classList.remove('hidden');
 
             button.addEventListener('click', () => {
                 localStorage.removeItem('USER');
                 location = './';
-            });
+            }
+            
+            );
+ 
         }
     }
 
@@ -17,16 +25,18 @@ class Header extends Component {
         const title = this.props.title || 'NagMe';
 
         return /*html*/`
-            <header>
-            <nav >
-                <a id="item1" href="./">Home</a>
-                <a id="item2" href="./about-us.html">About Us</a>
-                <button id="item3" class="log-out hidden">Log Out</button>
-         </nav>
-            <div class="logo">
-                <img class="nav-logo" src="./assets/2.png">
-              </div>
-                </header>
+        <header class="header">
+        <nav>
+        <span class="logged-in-as"></span>
+            <a id="item1" href="./">Home</a>
+            <a id="item3"  href="./list.html">Nags</a>
+            <a id="item2" href="./about-us.html">About Us</a>
+            <button id="item3" class="log-out hidden">Log Out</button>
+        </nav>
+        <div class="logo">
+            <img class="nav-logo" src="./assets/2.png">
+        </div>
+    </header> 
         `;
     }
 }
