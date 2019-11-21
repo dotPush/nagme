229af7606345a24e2129982549c29229c8baf173
drmartell <drmartell@gmail.com>
2019-11-21 00:00:50 -0800

    stretch fields added to schema

--- data/create-tables.js
+++ data/create-tables.js
@@ -1,43 +1,53 @@
 const client = require('../lib/client');
 
-// async/await needs to run in a function
-run();
+( //IIFE
+    async() => {
+        try {
+            // run a query to create tables
+            await client.query(`
+                CREATE TABLE users (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    email VARCHAR(256) NOT NULL,
+                    display_name VARCHAR(256) NOT NULL,
+                    password_hash VARCHAR(512) NOT NULL,
+                    push_api_key VARCHAR(256),
+                    email_to_SMS VARCHAR(256),
+                    alert_email VARCHAR(256)
+                );
+                
+                CREATE TABLE nags (
+                    id SERIAL PRIMARY KEY NOT NULL,
+                    task VARCHAR(256) NOT NULL,
+                    notes VARCHAR(512),
+                    start_time TIME NOT NULL,
+                    end_time TIME,
+                    interval INT,
+                    minutes_after_hour INT,
+                    snoozed BOOLEAN,
+                    period VARCHAR(16),
+                    mon BOOLEAN,
+                    tue BOOLEAN,
+                    wed BOOLEAN,
+                    thu BOOLEAN,
+                    fri BOOLEAN,
+                    sat BOOLEAN,
+                    sun BOOLEAN,
+                    recurs BOOLEAN,
+                    complete BOOLEAN,
+                    id_string VARCHAR(30),
+                    user_id INT NOT NULL REFERENCES users(id)
+                );
+            `);
 
-async function run() {
-
-    try {
-        // run a query to create tables
-        await client.query(`
-        CREATE TABLE users (
-            id SERIAL PRIMARY KEY NOT NULL,
-            push_api_key VARCHAR(256),
-            email VARCHAR(256) NOT NULL,
-            display_name VARCHAR(256) NOT NULL,
-            password_hash VARCHAR(512) NOT NULL
-        );
-        
-        CREATE TABLE nags (
-                id SERIAL PRIMARY KEY NOT NULL,
-                task VARCHAR(256) NOT NULL,
-                notes VARCHAR(512),
-                start_time TIME NOT NULL,
-                interval INT,
-                period VARCHAR(16),
-                user_id INT NOT NULL REFERENCES users(id)
-            );
-
-            
-        `);
-
-        console.log('create tables complete');
+            console.log('create tables complete');
+        }
+        catch (err) {
+            // problem? let's see the error...
+            console.log(err);
+        }
+        finally {
+            // success or failure, need to close the db connection
+            client.end();
+        }
     }
-    catch (err) {
-        // problem? let's see the error...
-        console.log(err);
-    }
-    finally {
-        // success or failure, need to close the db connection
-        client.end();
-    }
-
-}
+)();
