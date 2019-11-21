b4251de45b489dca514656ec7fec15008204bfe5
drmartell <drmartell@gmail.com>
2019-11-20 23:17:42 -0800

    adds psuedorandom id generation function

---
+++ public/util/getIdString.js
@@ -0,0 +1,10 @@
+//https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+export const getIdString = length => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
