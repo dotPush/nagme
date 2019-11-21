d8b61206c847316851e106a81788b92930fe386c
Nikki <nikki.satopdx@gmail.com>
2019-11-21 15:30:19 -0800

    responsive website

--- public/common/Header.js
+++ public/common/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
                 <button id="item3" class="log-out hidden">Log Out</button>
          </nav>
             <div class="logo">
-                <img class="nav-logo" src="./assets/2.png">
+                <img class="nav-logo" src="./assets/NM.png">
               </div>
                 </header>
         `;
