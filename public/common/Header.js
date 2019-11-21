f2be3495b57c6c16d6a78d542ecd3b8bec6ba43f
Nikki <nikki.satopdx@gmail.com>
2019-11-21 15:38:06 -0800

    merged

--- public/common/Header.js
+++ public/common/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
                 <button id="item3" class="log-out hidden">Log Out</button>
          </nav>
             <div class="logo">
-                <img class="nav-logo" src="./assets/NM.png">
+                <img class="nav-logo" src="./assets/NMBigLogo.png">
               </div>
                 </header>
         `;
