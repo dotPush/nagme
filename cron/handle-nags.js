eba7153db3745d9a2c6c51b8b98ef58890eee1aa
drmartell <drmartell@gmail.com>
2019-11-21 00:21:50 -0800

    explicit SELECT and ▲ dayofweek logic

--- cron/handle-nags.js
+++ cron/handle-nags.js
@@ -10,35 +10,65 @@ const fetchWithError = async(url, options) => {
     else throw data.error;
 };
 
-const getAllNags = async()=>{
+const getAllNags = async() => {
     try {
         const result = await client.query(`
-            SELECT *, users.id AS "deleteId",
-            push_api_key AS "pushApiKey" 
+            SELECT
+            task,
+            notes,
+            start_time AS "startTime"
+            end_time AS "endTime"
+            interval,
+            minutes_after_hour AS "minutesAfterHour",
+            snoozed,
+            period,
+            mon,
+            tue,
+            wed,
+            thu,
+            fri,
+            sat,
+            sun,
+            recurs,
+            complete,
+            id_string AS "idString"
+            users.id AS "deleteId",
+            push_api_key AS "pushApiKey",
             FROM users JOIN nags
             ON users.id = nags.user_id;
         `,);
         return result.rows;
     }
-    catch (err) console.log(err);
-}
+    catch (err) { console.log(err); }
+};
 
-//1-7 where 1 is Monday and 7 is Sunday
-const isDayOfWeek = dayNumsArr => dayNumsArr.includes(moment().isoWeekday());
+// 1-7 where 1 is Monday and 7 is Sunday
+// days are true by default
+const isDayOfWeek = nag => {
+    [
+        nag.mon && 1,
+        nag.tue && 2,
+        nag.wed && 3,
+        nag.thu && 4,
+        nag.fri && 5,
+        nag.sat && 6,
+        nag.sun && 7,
+    ]
+        .includes(moment().isoWeekday());
+};
 
-const isTimeForNag = (startTime, endTime = -1, dayNumsArr = [], snoozed = false) => {
-    const now = moment().format("HH:mm");
-    const nowTime = moment.utc(now, "HH:mm");
-    const startTime = moment.utc(nag.start_time,"HH:mm:ss");
-    const diff = moment.duration(nowTime.diff(startTime)).asMinutes();
+const isTimeForNag = (nag, dayNumsArr = [], snoozed = false) => {
+    const now = moment();
+    const nowTime = moment.utc(now, 'HH:mm');
+    const diff = moment.duration(nowTime.diff(nag.startTime)).asMinutes();
     return (                                                // return true if:
         !snoozed &&                                         // nag is not snoozed
-        nowTime.isAfter(startTime) &&                       // and it is after start time
-        (endTime > -1 && nowTime.isBefore(endTime)) &&      // and if there is an end time and we haven't exceeded it
+        nowTime.isAfter(nag.startTime) &&                   // and it is after start time
+        (nag.endTime && nowTime.isBefore(nag.endTime)) &&   // and if there is an end time and we haven't exceeded it
         (dayNumsArr && isDayOfWeek) &&                      // and there are days selected and this is one of them
         (
             diff % nag.interval === 0 ||                    // and this is one of the regularly recurring time intervals of a requested nag
-            moment().minutes() === minutesAfterTheHour      // or it is one of the number of minutes after the hour
+            moment().minutes() === nag.minutesAfterTheHour      // or it is one of the number of minutes after the hour
         )
     );
 };
@@ -46,12 +76,12 @@ const isTimeForNag = (startTime, endTime = -1, dayNumsArr = [], snoozed = false)
 const sendNags = async() => {
     const allNags = await getAllNags();
     allNags.forEach(async nag => {
-        const startTime = moment.utc(nag.start_time,"HH:mm");
+        nag.startTime = moment.utc(nag.start_time, 'HH:mm');
         if (
             nag.pushApiKey &&
             nag.pushApiKey.length === 30 &&
-            isTimeForNag(startTime) === 0)
-            {
+            isTimeForNag(nag) === 0)
+        {
             try {
                 const url = `https://api.pushover.net/1/messages.json`;
                 return await fetchWithError(url, {
@@ -68,10 +98,8 @@ const sendNags = async() => {
                     })        
                 });
             }
-            catch (err) console.log('error ' + err);
+            catch (err) { console.log('error ' + err); }
         }
-        
     });
-
 };
 exports.sendNags = sendNags;
